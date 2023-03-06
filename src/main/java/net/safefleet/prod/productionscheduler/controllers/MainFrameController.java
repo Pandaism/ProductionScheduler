8c54f2beb62bbf969d136981986bf9a818cca559
Pandaism <mnguyen@safefleet.net>
2023-03-06 15:25:38 -0600

    Enhance visibility on font Fixed bug where the properties file would get overridden every time the software launches

--- src/main/java/net/safefleet/prod/productionscheduler/controllers/MainFrameController.java
+++ src/main/java/net/safefleet/prod/productionscheduler/controllers/MainFrameController.java
@@ -1,7 +1,10 @@
 package net.safefleet.prod.productionscheduler.controllers;
 
+import javafx.beans.binding.Bindings;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import net.safefleet.prod.productionscheduler.data.SalesOrder;
 import net.safefleet.prod.productionscheduler.thread.AutoScrollingThread;
 import net.safefleet.prod.productionscheduler.thread.DateUpdaterThread;
@@ -46,44 +49,67 @@ public class MainFrameController {
         this.fifthTable.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
 
         TableColumn monSOCol = new TableColumn("SO");
+        monSOCol.setCellFactory(param -> new SaleOrderCell());
+        monSOCol.maxWidthProperty().bind(this.firstTable.widthProperty().multiply(0.3));
+        monSOCol.minWidthProperty().bind(this.firstTable.widthProperty().multiply(0.3));
         monSOCol.setCellValueFactory(new PropertyValueFactory<>("salesOrderID"));
-
         TableColumn monPIDCol = new TableColumn("Parts");
         monPIDCol.setCellFactory(param -> new PIDCell());
-        TableColumn monQuantityCol = new TableColumn("Quantity");
+        TableColumn monQuantityCol = new TableColumn("Qty");
+        monQuantityCol.maxWidthProperty().bind(this.firstTable.widthProperty().multiply(0.2));
+        monQuantityCol.minWidthProperty().bind(this.firstTable.widthProperty().multiply(0.2));
         monQuantityCol.setCellFactory(param -> new QuantityCell());
-
         this.firstCol.getColumns().addAll(monSOCol, monPIDCol, monQuantityCol);
 
         TableColumn tuesSOCol = new TableColumn("SO");
+        tuesSOCol.setCellFactory(param -> new SaleOrderCell());
+        tuesSOCol.maxWidthProperty().bind(this.firstTable.widthProperty().multiply(0.3));
+        tuesSOCol.minWidthProperty().bind(this.firstTable.widthProperty().multiply(0.3));
         tuesSOCol.setCellValueFactory(new PropertyValueFactory<>("salesOrderID"));
         TableColumn tuesPIDCol = new TableColumn("Parts");
         tuesPIDCol.setCellFactory(param -> new PIDCell());
-        TableColumn tuesQuantityCol = new TableColumn("Quantity");
+        TableColumn tuesQuantityCol = new TableColumn("Qty");
+        tuesQuantityCol.maxWidthProperty().bind(this.firstTable.widthProperty().multiply(0.2));
+        tuesQuantityCol.minWidthProperty().bind(this.firstTable.widthProperty().multiply(0.2));
         tuesQuantityCol.setCellFactory(param -> new QuantityCell());
         this.secondCol.getColumns().addAll(tuesSOCol,tuesPIDCol, tuesQuantityCol);
 
         TableColumn wedSOCol = new TableColumn("SO");
+        wedSOCol.setCellFactory(param -> new SaleOrderCell());
+        wedSOCol.maxWidthProperty().bind(this.firstTable.widthProperty().multiply(0.3));
+        wedSOCol.minWidthProperty().bind(this.firstTable.widthProperty().multiply(0.3));
         wedSOCol.setCellValueFactory(new PropertyValueFactory<>("salesOrderID"));
         TableColumn wedPIDCol = new TableColumn("Parts");
         wedPIDCol.setCellFactory(param -> new PIDCell());
-        TableColumn wedQuantityCol = new TableColumn("Quantity");
+        TableColumn wedQuantityCol = new TableColumn("Qty");
+        wedQuantityCol.maxWidthProperty().bind(this.firstTable.widthProperty().multiply(0.2));
+        wedQuantityCol.minWidthProperty().bind(this.firstTable.widthProperty().multiply(0.2));
         wedQuantityCol.setCellFactory(param -> new QuantityCell());
         this.thirdCol.getColumns().addAll(wedSOCol,wedPIDCol, wedQuantityCol);
 
         TableColumn thursSOCol = new TableColumn("SO");
+        thursSOCol.setCellFactory(param -> new SaleOrderCell());
+        thursSOCol.maxWidthProperty().bind(this.firstTable.widthProperty().multiply(0.3));
+        thursSOCol.minWidthProperty().bind(this.firstTable.widthProperty().multiply(0.3));
         thursSOCol.setCellValueFactory(new PropertyValueFactory<>("salesOrderID"));
         TableColumn thursPIDCol = new TableColumn("Parts");
         thursPIDCol.setCellFactory(param -> new PIDCell());
-        TableColumn thursQuantityCol = new TableColumn("Quantity");
+        TableColumn thursQuantityCol = new TableColumn("Qty");
+        thursQuantityCol.maxWidthProperty().bind(this.firstTable.widthProperty().multiply(0.2));
+        thursQuantityCol.minWidthProperty().bind(this.firstTable.widthProperty().multiply(0.2));
         thursQuantityCol.setCellFactory(param -> new QuantityCell());
         this.fourthCol.getColumns().addAll(thursSOCol,thursPIDCol, thursQuantityCol);
 
         TableColumn friSOCol = new TableColumn("SO");
+        friSOCol.setCellFactory(param -> new SaleOrderCell());
+        friSOCol.maxWidthProperty().bind(this.firstTable.widthProperty().multiply(0.3));
+        friSOCol.minWidthProperty().bind(this.firstTable.widthProperty().multiply(0.3));
         friSOCol.setCellValueFactory(new PropertyValueFactory<>("salesOrderID"));
         TableColumn friPIDCol = new TableColumn("Parts");
         friPIDCol.setCellFactory(param -> new PIDCell());
-        TableColumn friQuantityCol = new TableColumn("Quantity");
+        TableColumn friQuantityCol = new TableColumn("Qty");
+        friQuantityCol.maxWidthProperty().bind(this.firstTable.widthProperty().multiply(0.2));
+        friQuantityCol.minWidthProperty().bind(this.firstTable.widthProperty().multiply(0.2));
         friQuantityCol.setCellFactory(param -> new QuantityCell());
         this.fifthCol.getColumns().addAll(friSOCol,friPIDCol, friQuantityCol);
 
@@ -95,9 +121,42 @@ public class MainFrameController {
                         new ExpandableReaderThread.TableContainer(this.fourthTable, this.fourthCol),
                         new ExpandableReaderThread.TableContainer(this.fifthTable, this.fifthCol)
                 ), 0, 30, TimeUnit.MINUTES);
+
+
+    }
+
+    private static class SaleOrderCell extends TableCell<SalesOrder, String> {
+        private final Label label;
+
+        public SaleOrderCell() {
+            this.label = new Label();
+            this.label.setFont(Font.font(24));
+            this.setGraphic(this.label);
+            this.setContentDisplay(ContentDisplay.GRAPHIC_ONLY);
+        }
+        @Override
+        protected void updateItem(String item, boolean empty) {
+            super.updateItem(item, empty);
+
+            if(item != null || !empty) {
+                if(item != null) {
+                    this.label.setText(item.substring(2));
+                }
+            } else {
+                this.label.setText(null);
+            }
+        }
     }
 
     private static class QuantityCell extends TableCell<SalesOrder, Integer> {
+        private final Label label;
+
+        public QuantityCell() {
+            this.label = new Label();
+            this.label.setFont(Font.font(23));
+            this.setGraphic(this.label);
+            this.setContentDisplay(ContentDisplay.GRAPHIC_ONLY);
+        }
         @Override
         protected void updateItem(Integer item, boolean empty) {
             super.updateItem(item, empty);
@@ -105,25 +164,36 @@ public class MainFrameController {
             if (index >= 0 && index < getTableView().getItems().size()) {
                 SalesOrder so = getTableView().getItems().get(index);
                 List<Integer> quantities = so.getParts().stream().map(SalesOrder.Parts::getQuantities).collect(Collectors.toList());
-                setText(String.join("\n", quantities.stream().map(Object::toString).collect(Collectors.toList())));
+                this.label.setText(quantities.stream().map(Object::toString).collect(Collectors.joining("\n")));
             } else {
-                setText(null);
+                this.label.setText(null);
             }
         }
     }
 
     private static class PIDCell extends TableCell<SalesOrder, String> {
+        private final Label label;
+
+        public PIDCell() {
+            this.label = new Label();
+            this.label.setFont(Font.font(23));
+            this.setGraphic(this.label);
+            this.setContentDisplay(ContentDisplay.GRAPHIC_ONLY);
+        }
+
         @Override
         protected void updateItem(String item, boolean empty) {
             super.updateItem(item, empty);
             int index = getIndex();
             if (index >= 0 && index < getTableView().getItems().size()) {
                 SalesOrder so = getTableView().getItems().get(index);
-                List<String> pids = so.getParts().stream().map(SalesOrder.Parts::getId).collect(Collectors.toList());
-                setText(String.join("\n", pids));
+                List<String> pids = so.getParts().stream().map(parts -> parts.getId().trim()).collect(Collectors.toList());
+                this.label.setText(String.join("\n", pids));
             } else {
-                setText(null);
+                this.label.setText(null);
             }
+
+
         }
     }
 }
--- src/main/java/net/safefleet/prod/productionscheduler/data/files/PropertiesFile.java
+++ src/main/java/net/safefleet/prod/productionscheduler/data/files/PropertiesFile.java
@@ -14,20 +14,21 @@ public class PropertiesFile extends DataFile<Properties> {
 
     @Override
     protected void writeDefaults() {
-        OutputStream outputStream;
-        try {
-            outputStream = new FileOutputStream(super.file);
-            this.properties.setProperty("db.url", "localhost");
-            this.properties.setProperty("db.database", "database");
-            this.properties.setProperty("db.user", "user");
-            this.properties.setProperty("db.password", "password");
+        if(!super.file.exists()) {
+            OutputStream outputStream;
+            try {
+                outputStream = new FileOutputStream(super.file);
+                this.properties.setProperty("db.url", "localhost");
+                this.properties.setProperty("db.database", "database");
+                this.properties.setProperty("db.user", "user");
+                this.properties.setProperty("db.password", "password");
 
-            this.properties.store(outputStream, null);
-            outputStream.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+                this.properties.store(outputStream, null);
+                outputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-
     }
 
     @Override
